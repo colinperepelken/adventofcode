f69f4989095b1ad10ee0004b683a0d44c601337a
Colin Perepelken <colin@sparelabs.com>
2022-12-04 12:04:27 -0800

    Day 4 Part 1 in Elixir

---
+++ 2022/day4/part1.exs
@@ -0,0 +1,17 @@
+list_contains = &(Enum.count(Enum.at(&1, 1) -- Enum.at(&1, 0)) == 0 or Enum.count(Enum.at(&1, 0) -- Enum.at(&1, 1)) == 0)
+build_range = &(Enum.to_list(Enum.at(&1, 0)..Enum.at(&1, 1)))
+
+{:ok, body} = File.read("input.txt")
+body
+|> String.split("\n", trim: true)
+|> Enum.map(
+  fn line -> String.split(line, ",")
+  |> Enum.map(
+    fn side -> String.split(side, "-")
+    |> Enum.map(fn num -> elem(Integer.parse(num), 0) end)
+    |> build_range.()
+  end)
+  |> list_contains.()
+end)
+|> Enum.count(&(&1))
+|> IO.inspect(charlists: :as_lists)
